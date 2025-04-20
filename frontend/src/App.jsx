6dcc45dfca6f34f59da7371dd07152500ebb132f
Darshan <darshanisadev@gmail.com>
2025-04-20 19:24:40 +0530

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import InvoiceProcessorVisualization from './components/InvoiceProcessorVisualization';
+
+function App() {
+  return (
+    <div className="App">
+      <InvoiceProcessorVisualization />
+    </div>
+  );
+}
+
+export default App;
